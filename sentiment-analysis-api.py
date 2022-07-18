4a4d5f0d07f063370eadce69ea4db3cf76325830
Louie Cai <louiecai@outlook.com>
2022-07-17 19:38:13 -0700

    Cleaned up the unused variables and method calls.

--- sentiment-analysis-api.py
+++ sentiment-analysis-api.py
@@ -1,21 +1,14 @@
-import os
-
-import numpy as np
 import torch
 from fastapi import FastAPI
 
-from model_utils import get_model, Namespace
+from model_utils import Namespace
+from model import RNN
 
 app = FastAPI()
 
-model_path = 'models/2022-07-16 15:27:27.014567'
-args = eval(open(os.path.join(model_path, 'config.txt')).read())
-torch.manual_seed(args.random_seed)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed_all(args.random_seed)
-np.random.seed(args.random_seed)
+model_path = 'models/2022-07-17 15:35:54.392705'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = get_model(args.model).load(model_path)
+model = RNN.load(model_path)
 
 
 @app.get("/")
@@ -25,4 +18,4 @@ def root():
 
 @app.get("/eval")
 def eval_text(text: str = '') -> dict:
-    return {'sentiment': model.predict(text, device)[0] if text != '' else 'Please enter text to evaluate'}
+    return {'sentiment': model.predict(text, device)[0] if text != '' else None}
