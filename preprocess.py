35d351142e16eacf4e85d41538e89315bacfb536
Louie Cai <louiecai@outlook.com>
2022-07-17 19:37:15 -0700

    Documentations added.

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,13 @@ import torch
 import os
 
 
-def get_datasets(path: str = os.getcwd() + '/data/airline_sentiment_analysis', vocab_size: int = 10000) -> tuple:
+def get_datasets(path: str = os.getcwd() + '/data/airline_sentiment_analysis.csv', vocab_size: int = 10000) -> tuple:
+    """
+    Returns the train and valid datasets from a `.csv` file. Tokenizes the text using the `spacy` library.
+    :param path: path to data folder
+    :param vocab_size: size of vocabulary
+    :return: train_dataset, valid_dataset
+    """
     text_field = data.Field(tokenize='spacy', tokenizer_language='en_core_web_sm', lower=True)
     label_field = data.LabelField(dtype=torch.int64)
 
@@ -19,6 +25,13 @@ def get_datasets(path: str = os.getcwd() + '/data/airline_sentiment_analysis', v
 
 def get_dataloaders(train_dataset: data.TabularDataset, valid_dataset: data.TabularDataset, batch_size: int = 32,
                     device: torch.device = torch.device('cpu')) -> tuple:
+    """
+    Returns the train and valid dataloaders.
+    :param train_dataset: train dataset
+    :param valid_dataset: valid dataset
+    :param batch_size: batch size
+    :param device: device
+    """
     train_loader, valid_loader = data.BucketIterator.splits([train_dataset, valid_dataset], batch_size=batch_size,
                                                             sort_within_batch=False, device=device,
                                                             sort_key=lambda x: len(x.text))
